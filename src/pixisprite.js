eb3966cddc77c3896d544e4b3e64d82e758cc0ee
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2017-07-04 16:04:12 +0900

    create pixisprite

---
+++ src/pixisprite.js
@@ -0,0 +1,43 @@
+(function(phina) {
+  phina.define('phina.pixi.PixiSprite', {
+    superClass: phina.pixi.PixiElement,
+
+    init: function(image, x, y, width, height) {
+      this.superInit({
+        pixiObject: new PIXI.Sprite(),
+      });
+
+      if (image) {
+        this.setImage(image, x, y, width, height);
+      }
+    },
+
+    setImage: function(image, x, y, width, height) {
+      if (typeof image === 'string') {
+        image = phina.asset.AssetManager.get('pixi', image);
+      }
+      if (typeof x === 'number') {
+        image = image.createFrame(x, y, width, height);
+      }
+      this._image = image;
+
+      this.pixiObject.texture = image.pixiTexture;
+
+      return this;
+    },
+
+    setFrame: function(x, y, width, height) {
+      this._image = this._image.createFrame(x, y, width, height);
+      return this;
+    },
+
+    _accessor: {
+      image: {
+        get: function() { return this._image; },
+        set: function(v) {
+          this.setImage(v);
+        }
+      },
+    }
+  });
+}(phina));
