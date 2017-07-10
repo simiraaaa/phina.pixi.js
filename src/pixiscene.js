09d6bb44007e4f132059d2bed3786d6c2f399e22
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2017-07-10 17:45:13 +0900

    fix pixiscene

--- src/pixiscene.js
+++ src/pixiscene.js
@@ -10,7 +10,7 @@
       this.superInit(options = (options || {}).$safe(phina.pixi.PixiScene.defaults));
 
       // this.backgroundColor = (options.backgroundColor) ? options.backgroundColor : null;
-      
+
       this.gridX = phina.util.Grid(options.width, 16);
       this.gridY = phina.util.Grid(options.height, 16);
 
@@ -38,10 +38,6 @@
       return this;
     },
 
-    hitTest: function() {
-      return true;
-    },
-
     _update: function() {
       if (this.update) {
         this.update();
@@ -55,8 +51,29 @@
       defaults: {
         width: 640,
         height: 960,
+        boundingType: 'none',
       },
-    }
+    },
+
+    _accessor: {
+      width: {
+        get: function() {
+          return this.pixiObject._width / this._scale.x;
+        },
+        set: function(v) {
+          this.pixiObject.width = v * this._scale.x;
+        },
+      },
+
+      height: {
+        get: function() {
+          return this.pixiObject._height / this._scale.y;
+        },
+        set: function(v) {
+          this.pixiObject.height = v * this._scale.y;
+        }
+      },
+    },
 
     
   });
