1be20f73ec8c1a9b1c0f26f8cdb32ac1f28f371e
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2017-07-11 17:52:57 +0900

    fix texture from canvas and shape

--- src/pixitexture.js
+++ src/pixitexture.js
@@ -22,7 +22,6 @@
         x = x.x;
       }
       var newTexture = phina.pixi.PixiTexture();
-      newTexture.phinaTexture = this.phinaTexture;
       newTexture.pixiTexture = new PIXI.Texture(this.pixiTexture.baseTexture, new PIXI.math.Rectangle(x, y, width, height));
 
       return newTexture;
@@ -31,6 +30,41 @@
     setKey: function(key) {
       this.key = key;
       return this;
+    },
+    
+    /**
+     * @param {HTMLCanvasElement} canvas
+     */
+    fromCanvas: function(canvas) {
+      this.pixiTexture = PIXI.Texture.fromCanvas(canvas);
+      return this;
+    },
+
+    /**
+     * @param {phina.display.Shape} shape
+     */
+    fromShape: function(shape) {
+      if(shape._dirtyDraw) {
+        shape.render(shape.canvas);
+      }
+      return this.fromCanvas(shape.canvas.domElement);
+    },
+
+    _static: {
+      
+      /**
+       * @param {HTMLCanvasElement} canvas
+       */
+      fromCanvas: function(canvas) {
+        return phina.pixi.PixiTexture().fromCanvas(canvas);
+      },
+
+      /**
+       * @param {phina.display.Shape} shape
+       */
+      fromShape: function(shape) {
+        return phina.pixi.PixiTexture().fromShape(shape);
+      }
     }
     
   });
