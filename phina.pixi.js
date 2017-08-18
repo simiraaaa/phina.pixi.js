c7ae677b285dc19ed5acc86d3f243b50815fc047
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2017-08-18 15:41:02 +0900

    bugfix mulMatVec2

--- phina.pixi.js
+++ phina.pixi.js
@@ -109,8 +109,8 @@ phina.pixi = {
       },
 
       mulMatVec2: function(mat, vec2) {
-        var vx = mat.a*vec2.x + mat.b*vec2.y + mat.tx;
-        var vy = mat.c*vec2.x + mat.d*vec2.y + mat.ty;
+        var vx = mat.a*vec2.x + mat.c*vec2.y + mat.tx;
+        var vy = mat.b*vec2.x + mat.d*vec2.y + mat.ty;
         
         return phina.geom.Vector2(vx, vy);
       },
